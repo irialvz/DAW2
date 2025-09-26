3eeaa175828ad7bb014c6e156ce7a9b3ea9d1a74
iria <iriiachan11@gmail.com>
2025-09-26 12:00:42 +0200

    docker fin de unidad

---
+++ DesarrolloServer/php/app/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>prueba</title>
+</head>
+<body>
+<h1>Holaaaaaaaaaa mundo</h1>
+<table>
+    <tr>
+        <td>Decimal</td>
+        <td>Hexadec</td>
+        <td>Binario</td>
+        <td>Octal</td>
+    </tr>
+<?php
+for ($numero = 1; $numero <= 16; $numero++){
+/*    $hexadecimal = dechex($numero);
+    $binario = decbin($numero);
+    $octal = decoct($numero);
+
+    echo "<tr>";
+    echo "<td>$numero</td><td>$hexadecimal</td><td>$binario</td><td>$octal</td>";
+    echo "</tr>";*/
+
+}
+
+?>
+</table>
+
+
+</body>
+
+</html>
