edc34b77413264fb12ac72d88149242a1933953a
iria <iriiachan11@gmail.com>
2025-10-01 13:25:29 +0200

    me cago ent odo

--- DesarrolloCliente/UNIDAD2/2_6TipoDatos/ejercicio.js
+++ DesarrolloCliente/UNIDAD2/2_6TipoDatos/ejercicio.js
@@ -3,9 +3,15 @@ let primerApellido = prompt("Introduce tu priemer apellido");
 let nombreCompleto = nombreUsuario.concat(" " + primerApellido);
 document.getElementById("nombreCompleto").innerHTML += nombreCompleto;
 
-let edad = prompt("Edad? ");
-const d = new Date();
-let nacimiento = d.getFullYear - edad;
-document.getElementById("edad").innerHTML += nacimiento;
+const dia = new Date();
+let diaNacimiento = prompt("dia de nacimiento? ");
+let mesNacimiento = prompt("mes de nacimiento? ");
+let anyoNacimiento = prompt("año de nacimiento? ");
+diaNacimiento = diaNacimiento - dia.getDate() ;
+mesNacimiento = mesNacimiento -dia.getMonth() ;
+anyoNacimiento = dia.getFullYear() - anyoNacimiento;
 
+document.getElementById("anyo").innerHTML += anyoNacimiento.toString().split("-");
+document.getElementById("mes").innerHTML += mesNacimiento.toString().split("-");
+document.getElementById("dia").innerHTML += diaNacimiento.toString().split("-");
 
