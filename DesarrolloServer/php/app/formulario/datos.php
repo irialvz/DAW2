561b673b2f612f488b670e8471bd2a4119666ae4
iria <iriiachan11@gmail.com>
2025-10-09 10:13:45 +0200

    formularios php

--- DesarrolloServer/php/app/formulario/datos.php
+++ DesarrolloServer/php/app/formulario/datos.php
@@ -2,21 +2,37 @@
 $numero1=$_GET["numero1"];
 $operador =$_GET["operador"];
 $numero2 =$_GET["numero2"];
-$total = 0;
-switch ($operador) {
-    case '+':
-        $total = $numero1+$numero2;
-        break;
-    case '-':
-        $total = $numero1-$numero2;
-        break;
-    case '*':
-        $total = $numero1*$numero2;
-        break;
-    case '/':
-        $total = $numero1.$numero2;
-        break;
+$operacion = "$numero1 $operador $numero2";
+//validar
+if(!is_int($numero1) || !is_int($numero2)){
+    $msj = "El valor ingresado no es valido.Operacion $operacion imposible";
+};
+if($operador == ":" && $numero2 = 0){
+    $msj = "Division $operacion  imposible";
+};
+//En funcion del operador realiza el calculo
+if ($msj == ""){
+    $msj = match($operador){
+        '*' => $numero1*$numero2,
+        '+' => $numero1+$numero2,
+        '-' => $numero1-$numero2,
+        '/' => $numero1/$numero2,
+    };
 }
-echo "<h1>$total
-</h1>";
+
+
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1><?= $msj?></h1>
+<a href="formulario.php">Vovler</a>
+</body>
+</html>
--- DesarrolloServer/php/app/formulario/datos2.php
+++ DesarrolloServer/php/app/formulario/datos2.php
@@ -1,1 +1,26 @@
 <?php
+$NOMBRE=htmlspecialchars($_POST["nombre"]);
+$PASSWORD =htmlspecialchars($_POST["password"]);
+$EDAD = filter_input(INPUT_POST, "edad", FILTER_SANITIZE_NUMBER_INT);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<fieldset>
+    <legend>Datos Personales</legend>
+    <h1>Nombre <?="$NOMBRE" ?></h1>
+    <h1>Password <?="$PASSWORD" ?></h1>
+    <h1>EDAD <?="$EDAD" ?></h1>
+    <a href="datos_personales.php">volver</a>
+</fieldset>
+
+
+</body>
+</html>
