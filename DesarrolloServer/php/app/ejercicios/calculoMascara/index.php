1e039beea6396bd5732eeb8cb8f4cd1eb244c2a9
iria <iriiachan11@gmail.com>
2025-10-20 10:46:12 +0200

    operadores

--- DesarrolloServer/php/app/ejercicios/calculoMascara/index.php
+++ DesarrolloServer/php/app/ejercicios/calculoMascara/index.php
@@ -1,1 +1,55 @@
 <?php
+if (isset($_POST["submit"])) {
+    $mask1 = $_POST["masc1"]??0;
+    $mask2 = $_POST["masc2"]??0;
+    $mask3 = $_POST["masc3"]??0;
+    $mask4 = $_POST["masc4"]??0;
+    //recoger ip
+    $ip1 = $_POST["ip1"]??0;
+    $ip2 = $_POST["ip2"]??0;
+    $ip3 = $_POST["ip3"]??0;
+    $ip4 = $_POST["ip4"]??0;
+
+    $red1= $mask1 & $ip1;
+    $red2= $mask2 & $ip2;
+    $red3= $mask3 & $ip3;
+    $red4= $mask4 & $ip4;
+
+
+    $mensaje = printf("%b & %b = %b",$mask1,$ip1,$red1);
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="index.php" method="post">
+
+    Mascara de red
+    <input min="0" max="255" size="3" type="number" value="255" name="masc1">
+    <input min="0" max="255" size="3" type="number" value="255" name="masc2">
+    <input min="0" max="255" size="3" type="number"  value="255" name="masc3">
+    <input min="0" max="255" size="3" type="number" value="255" name="masc4">
+    <br>
+    Direccion IP
+    <input min="0" max="255" size="3" type="number" name="dir1">
+    <input min="0" max="255" size="3" type="number" name="dir2">
+    <input min="0" max="255" size="3" type="number" name="dir3">
+    <input min="0" max="255" size="3" type="number" name="dir4">
+    <br>
+    <button type="submit">Comprobar</button>
+</form>
+<?php
+if(!is_null($mensaje)):?>
+    <h1>RESULTADO</h1>
+    <?= $mensaje;endif?>
+
+</body>
+</html>
--- DesarrolloServer/php/app/operadores/index.php
+++ DesarrolloServer/php/app/operadores/index.php
@@ -1,1 +1,39 @@
 <?php
+echo "<h1>POTENCIAS CON BUCLE</h1>";
+for($a=1;$a<=10;$a++){
+    $potencia = 2**$a;
+    echo "<p>2 <sup>$a</sup> = $potencia</p>";
+}
+echo "<h1>POTENCIAS pow CON BUCLE</h1>";
+for($a=1;$a<=10;$a++){
+    $potencia = pow(2,$a);
+    echo "<p>2 <sup>$a</sup> = $potencia</p>";
+}
+
+echo "<h1>POTENCIAS CON DESPLAZAMIENTO</h1>";
+for($a=1;$a<=10;$a++){
+    $potencia = 1<<$a;
+    echo "<p>2 <sup>$a</sup> = $potencia</p>";
+}
+
+//visualizo un nombre en minuxuals y mayusculas y lo dejo en mayusuclas
+// si asignamos & en la variable del parametro,se quedara el resultado
+// de la funcion,en este caso se quedara en mayusculas
+function mayusculasReferencia(string &$nombre):void{
+    echo "<hr>";
+    echo "<h1>dentro de la funcion $nombre</h1>";
+    $nombre = strtoupper($nombre);
+    echo "<h1>dentro de la funcion $nombre</h1>";
+}
+
+$nombre = "Iria";
+mayusculasReferencia($nombre);
+echo "<h1>despuse de la funcion el nombre vale $nombre</h1>";
+
+//
+echo "<hr><h1>Calculo de mascaras</h1>";
+$numero1 = 128;
+$numero2 = 200;
+$and = $numero1 & $numero2;
+echo "<h1>$numero1 & $numero2 = $and</h1>";
+printf("<h1>%b & %b = %b</h1>",$numero1,$numero2,$and);
