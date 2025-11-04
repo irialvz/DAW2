92be461c5abbfbdc4505487faa5b279c4e42da6b
iria <iriiachan11@gmail.com>
2025-11-04 08:40:21 +0100

    la agenad d los webos

--- DesarrolloServer/php/app/array/ejerTelefonos/agenda.php
+++ DesarrolloServer/php/app/array/ejerTelefonos/agenda.php
@@ -1,17 +1,42 @@
 <?php
-$nombreContacto = $_POST["nombre"]??"";
+$nombreContacto = trim($_POST["nombre"]??"");
 $telefonoContacto = $_POST["telefono"]??"";
-$contactos = [];
+$telefonoContacto = (int) $telefonoContacto;
+$contactos = $_POST["contactos"]??[];
+
+
+function agenda (array &$contactos,string $nombre,int$telefono):void{
+    if (!array_key_exists($nombre, $contactos)) {
+        $contactos[$nombre] = $telefono;
+    } else if ($telefono === "") {
+        unset($contactos[$nombre]);
+    } else {
+        $contactos[$nombre] = $telefono;
+    }
+};
+function validar_error(array &$contactos,string $nombre,int$telefono):null|string{
+    $error=null;
+    $error = $nombre ==""? "El nombre no puede estar vacio <br/>":$error;
+    $error = is_numeric($telefono)? "El telefono debe ser numerico <br/>":$error;
+    $error = (!isset($contactos[$nombre])&&($telefono==""))? "No se puede eliminar un contacto sin numero <br/>":$error;
+    $error = (in_array($telefono,$contactos))? "No se puede eliminar un contacto sin numero <br/>":$error;
+    return $error;
+}
+
 if (isset($_POST['submit'])){
     if($nombreContacto != "" && $telefonoContacto != ""){
-        $telefonoContacto = (int) $telefonoContacto;
-        $contactos= agenda($nombreContacto, $telefonoContacto);
+
+        agenda($contactos,$nombreContacto, $telefonoContacto);
+        $cantidad = sizeof($contactos)==0 ? "sin" : +sizeof($contactos);
+        $plural = sizeof($contactos)==1 ? "contacto" : "contactos";
     }
 }
-if (isset($_POST['reset'])){
-    if (!empty($contactos)){
-        //todo bucle para unset cada valor
-    }
+$habilitarBoton = empty($contactos)? "disabled" : " ";
+if (isset($_POST['eliminar']) && !empty($contactos)){
+    // Se podrán borrar todos los contactos de la agenda. El botón borrar contactos sólo estará habilitado si hay contactos en la agenda
+        foreach ($contactos as $contacto){
+            unset($contactos[$contacto]);
+        }
 }
 ?>
 <!doctype html>
@@ -25,43 +50,41 @@ if (isset($_POST['reset'])){
 </head>
 <body>
     <header style="background-color:lightskyblue;padding:15px; color: black; display: flex;justify-content: center;">
-        <h1>Agenda de contactos con: //todo variable contador contactos // contactos</h1>
+        <h1>Agenda de contactos: <?= $cantidad??"sin contactos"?> <?=$plural??""?></h1>
     </header>
     <div style="display: flex; justify-content: space-between">
         <fieldset style="width: 30%">
             <legend>Nuevo contacto</legend>
+            <form action="agenda.php" method="post">
             Nombre <input type="text" name="nombre">
             Telefono <input type="text" name="telefono" >
+
             <button type="submit" name="submit">Añadir contacto</button>
-            <button type="reset" name="reset">Eliminar contacto</button>
+            <button  <?=$habilitarBoton ?> name="eliminar">Eliminar contacto</button>
+            </form>
         </fieldset>
         <div style="width: 50%;background-color:mediumslateblue;display: flex;justify-content: center; flex-direction: column;">
             <h1>Listado de contactos</h1>
-            <p> blablavbla</p>
+            <?php if (!empty($contactos)){
+                foreach ($contactos as $contacto => $telefono){
+                    echo "<input type='hidden' name='contactos[$contacto]' value='".$telefono."'>";
+                    echo "<table style='display: flex;justify-content: left;border: 3px solid black;width: 20%; '>";
+                    echo "<tr>";
+                    echo "<th>Nombre</th>";
+                    echo "<th>Telefono</th>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>" . $contacto . "</td>";
+                    echo "<td>" . $telefono . "</td>";
+                    echo "</tr>";
+                    echo "</table>";
+                }
+            }else {
+                echo "No hay registros";
+            };
+            ?>
         </div>
     </div>
 
 </body>
 </html>
-<?php
-
-function agenda(string $nombre,int$telefono):array{
-    $agenda = [];
-//    Se podrán borrar todos los contactos de la agenda.
-//    El botón borrar contactos sólo estará habilitado si hay contactos en la agenda
-    if (!array_key_exists($nombre, $agenda)){
-        // Si el nombre que se introdujo no existe en la agenda, y el número de teléfono no está vacío, se añadirá a la agenda.
-        $agenda[$nombre] = $telefono;
-    } else if (array_key_exists($nombre, $agenda) && is_null($telefono)){
-        //    Si el nombre que se introdujo ya existe en la agenda y no se indica número de teléfono, se eliminará de la agenda la entrada correspondiente a ese nombre.
-            unset($agenda[$nombre]);
-    }else if (array_key_exists($nombre, $agenda) && !is_null($telefono)){
-        //    Si el nombre que se introdujo ya existe en la agenda y se indica un número de teléfono, se sustituirá el número de teléfono anterior.
-        $agenda[$nombre] = $telefono;
-    }
-
-
-
-}
-
-?>
