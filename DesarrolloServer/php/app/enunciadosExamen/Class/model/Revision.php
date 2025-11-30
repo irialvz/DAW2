f51241eea5e4a5cbce6b96f5584ce54fc2185874
iria <iriiachan11>
2025-11-30 21:55:30 +0100

    php

---
+++ DesarrolloServer/php/app/enunciadosExamen/Class/model/Revision.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace enunciadosExamen\Class\enunciado;
+
+use Cassandra\Date;
+
+class Revision
+{
+    private string $fecha;
+    private float $costeBase;
+    private float $peso;
+    private string $diagnostico;
+    private string $veterinario;
+
+    public function __construct($fech,$peso,$diagnostico,$veterinario)
+    {
+        $this->fecha = $fech;
+        $this->costeBase = 30;
+        $this->peso= $peso;
+        $this->diagnostico=$diagnostico;
+        $this->veterinario=$veterinario;
+    }
+
+    public function calcularCosteFinal():float{
+        return $this->costeBase * rand(10,100);
+    }
+    /**
+     * @return string
+     */
+    public function getFecha(): string
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * @return float
+     */
+    public function getCosteBase(): float
+    {
+        return $this->costeBase;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDiagnostico(): string
+    {
+        return $this->diagnostico;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPeso(): float
+    {
+        return $this->peso;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVeterinario(): string
+    {
+        return $this->veterinario;
+    }
+
+}
